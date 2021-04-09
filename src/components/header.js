295f5ff881b564b47620d233eef95d590bc490bd
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-04-09 22:43:32 +0200

    header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className="container-fluid">
+    <nav className="d-flex justify-content-between">
+      <div>
+        <Link to="/" className="align-self-start">Home</Link>
+        <Link to="/" className="align-self-start">26 foto</Link>
+      </div>
+      <div>
+        <Link to="/" className="align-self-start">Credits</Link>
+        <Link to="/" className="align-self-start">EN/IT</Link>
+      </div>
+    </nav>
   </header>
 );
 
